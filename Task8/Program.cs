d79c097dff337d11cb396cffff62da3bb0e315d4
Alex Romadin <chrono001@mail.ru>
2022-06-28 12:05:34 +0300

    Task8 updated

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n, current;
+
+Console.WriteLine("Input number");
+n = Convert.ToInt32(Console.ReadLine());
+
+current = n % 2 == 0;
+
+while(current <= n);
+{
+    Console.Write(current + " ");
+    current++;
+}
