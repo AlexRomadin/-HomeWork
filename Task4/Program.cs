1e556f1e7d084643753f81dfb0b4ba24b558fbf2
Alex Romadin <chrono001@mail.ru>
2022-06-28 11:22:01 +0300

    Task4 completed

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-int a, b, c;
+int a, b, c, max;
 
 Console.WriteLine("Input 1 number");
 a = Convert.ToInt32(Console.ReadLine());
@@ -8,4 +8,12 @@ b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input 1 number");
 c = Convert.ToInt32(Console.ReadLine());
 
+max=a;
+
+if(b>max) max=b;
+if(c>max) max=c;
+
+{
+    Console.WriteLine(max);
+}
 
