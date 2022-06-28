7c707580829fc97cbd032d011ba9a9b33399cd00
Alex Romadin <chrono001@mail.ru>
2022-06-28 11:29:05 +0300

    Task4 corrected2

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Input 1 number");
 a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input 2 number");
 b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input 1 number");
+Console.WriteLine("Input 3 number");
 c = Convert.ToInt32(Console.ReadLine());
 
 max = a;
