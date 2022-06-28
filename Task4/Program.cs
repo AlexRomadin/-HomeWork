7f13d4d83b2b8467df43981c5a42f1e95fc63231
Alex Romadin <chrono001@mail.ru>
2022-06-28 11:09:08 +0300

    Task4 updated

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int a, b, c;
+
+Console.WriteLine("Input 1 number");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input 2 number");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input 1 number");
+c = Convert.ToInt32(Console.ReadLine());
+
+
