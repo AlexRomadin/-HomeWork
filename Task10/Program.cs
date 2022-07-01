8ba04753c447b8accb1acdfe58a80ac1adeeb89c
Alex Romadin <chrono001@mail.ru>
2022-07-01 10:37:17 +0300

    task 10 complete

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введи трёхзначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(Number);
+
+Console.WriteLine("вторая цифра этого числа "+ stringNumber[1]);
+
