a644618e71e85d9f7f6ddd5e6aea69996d39d821
Alex Romadin <chrono001@mail.ru>
2022-07-01 10:30:19 +0300

    task 15 complete

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+void WeekDay (int dayNumber) 
+{
+  if (day == 6 || day == 7) {
+  Console.WriteLine("Этот день выходной? -> да");
+  }
+  else 
+  
+  if (day < 1 || day > 7) {
+    Console.WriteLine("Это не день недели");
+  }
+  else Console.WriteLine("Этот день выходной? -> нет");
+}
+
+WeekDay(day);
