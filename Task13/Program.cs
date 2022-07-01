3982ddd9bd3004fdcb23a635f1621d66a7a47f52
Alex Romadin <chrono001@mail.ru>
2022-07-01 10:21:00 +0300

    task 13 completed

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number;
+
+Console.WriteLine("Input number: ");
+number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if(numberText.Length > 2)
+{
+    Console.WriteLine("Цифра № 3: " + numberText[2]);
+}
+else
+{
+    Console.WriteLine("Цифра № 3 отсутствует ^_^");
+}
