bee21974b529cb3a645d0bbe8bc4aff4b74fec49
Alex Romadin <chrono001@mail.ru>
2022-07-05 11:46:19 +0300

    task 19 complete

---
+++ Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Convert.ToString(Console.ReadLine());
+
+void CheckingNumber(string number)
+{
+  if (number[0] == number[4] && number[1] == number[3])
+  {
+    Console.WriteLine("Ваше число палиндром?: Да");
+  }
+  else Console.WriteLine("Ваше число палиндром?: Нет");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine("Введите правильное число");
