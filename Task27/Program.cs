c984492bee2d153b3ec2ba6fa5c286ec92a2ad73
Alex Romadin <chrono001@mail.ru>
2022-07-08 19:57:16 +0300

    task 27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+void NumSum();
+{
+    while (i > 0)
+    {
+        int num = i % 10;
+        i = i / 10;
+        sum = sum + num;
+    }
+}
+
+Console.WriteLine("сумма всех цифр равна: " + NumSum());
