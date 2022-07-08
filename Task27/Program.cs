827107b2f767527dcea2927104a2554755fabffa
Alex Romadin <chrono001@mail.ru>
2022-07-08 20:19:17 +0300

    task 27 complete

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,14 +2,11 @@
 int i = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 
-void NumSum();
+while (i > 0)
 {
-    while (i > 0)
-    {
-        int num = i % 10;
-        i = i / 10;
-        sum = sum + num;
-    }
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
 }
 
-Console.WriteLine("сумма всех цифр равна: " + NumSum());
+Console.WriteLine("сумма всех цифр равна: " + sum);
