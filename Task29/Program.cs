ad02bd1e4895ca297e1442f826d2d35ad8acce2a
Alex Romadin <chrono001@mail.ru>
2022-07-08 20:27:35 +0300

    task 29 added

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿void Main()
+        {
+            int size = 20;
+            int[] array = new int[20];
+            Random myRandom = new Random();
+ 
+            Console.WriteLine("Массив n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = myRandom.Next(0, 20);
+                Console.Write("{0} ", array[i]);
+            }
+ 
+        }
