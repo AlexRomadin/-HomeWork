71fc3286c2b33bff27273f24d4cc230914e02da4
Alex Romadin <chrono001@mail.ru>
2022-07-11 18:56:54 +0300

    task 27 remake

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,11 +1,18 @@
-﻿Console.WriteLine("введите число");
-int i = Convert.ToInt32(Console.ReadLine());
+﻿int FindNumbersSum(int a)
+{
 int sum = 0;
 
-while (i > 0)
-{
-int num = i % 10;
-i = i / 10;
-sum = sum + num;
+while (a > 0)
+
+    {
+    int num = a % 10;
+    a = a / 10;
+    sum += num;
+    }
+    return sum;
 }
-Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"сумма всех цифр в числе равна: {FindNumbersSum(i)}");
