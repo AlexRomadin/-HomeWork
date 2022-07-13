30e0732c82f7bad48a45d3e628870d43a5d79f2d
Alex Romadin <chrono001@mail.ru>
2022-07-13 21:58:48 +0300

    task 34 complete

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -12,6 +12,8 @@ void ShowArray(int[] array)
 {
     for(int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
+
+    Console.WriteLine();
 }
 
 Console.Write("Input size of array: ");
@@ -19,3 +21,11 @@ int size = Convert.ToInt32(Console.ReadLine());
 
 int[] array1 = CreateRandomArray(size);
 ShowArray(array1);
+
+int count = 0;
+
+for (int i = 0; i < array1.Length; i++)
+    if (array1[i] % 2 == 0)
+    count++;
+
+Console.WriteLine($"Всего {array1.Length} чисел, {count} из них чётные");
