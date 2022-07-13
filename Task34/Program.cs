224157ecebd15a9e56a218c3a5eba4bd6157c67e
Alex Romadin <chrono001@mail.ru>
2022-07-13 21:30:54 +0300

    task 34 added

---
+++ Task34/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] CreateRandomArray(int size)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(100, 1000);
+    
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+Console.WriteLine("Input size of array");
+int size = Convert.ToInt32(Console.ReadLine());
+
+CreateRandomArray(size);
+ShowArray(size);
