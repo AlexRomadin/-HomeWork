8e4c4b28dc05a0f1b5b116b75002686bb2fdbdda
Alex Romadin <chrono001@mail.ru>
2022-07-22 18:48:02 +0300

    task 64 complete

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,38 +1,18 @@
-﻿/*int NaturalNumber(int n, int m)
-{
-    if (n == m)
-        return n;
-    else
-        Console.Write($"{NaturalNumber(n, m + 1)}, ");
-    return m;
-}
-
-int InputInt(string output)
-{
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
-}
-
-int n = InputInt("Введите положительное число: ");
-int m = 1;
-if (n < 1)
-{
-    Console.WriteLine("Ввели неверное число!");
-}
-
-Console.WriteLine(NaturalNumber(n, m));*/
-
-void PrintNumber(int n, int count)
+﻿void PrintNumber(int n, int count)
 {
   if (count > n) return;
+  
   PrintNumber(n, count + 1);
+  
   Console.Write(count + ", ");
 }
 
 int InputNumbers(string input) 
 {
   Console.Write(input);
+
   int output = Convert.ToInt32(Console.ReadLine());
+  
   return output;
 }
 
